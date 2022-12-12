a864dac859a2fd39a3f374d9fdb283a7d37b3791
AaronNazareth <4nm20cs005@nmamit.in>
2022-12-12 20:22:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello Aaron lets build the amazon store</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD3bWGU4R9iWEnP6zytFsnKNDP6WdxS4Tw",
+    authDomain: "clone-d484f.firebaseapp.com",
+    projectId: "clone-d484f",
+    storageBucket: "clone-d484f.appspot.com",
+    messagingSenderId: "992526128738",
+    appId: "1:992526128738:web:576f1d1374f6dcef39e33b",
+    measurementId: "G-KE26KMFZL5"
+  };
