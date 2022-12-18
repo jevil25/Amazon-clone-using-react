9a376c044bf34087a05105cddd82a9de6e01f2e5
AaronNazareth <4nm20cs005@nmamit.in>
2022-12-18 16:29:55 +0530

    Added react states

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
+import React from 'react';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-          <Header />
-          <Routes>
-            <Route path="/checkout" element={[<Checkout />]} />
-            <Route path="/" element={[<Home/>]} />
-          </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <Router>
+        <div className="App">
+            <Header />
+            <Routes>
+            <Route path="/" element={<Home/>} />
+              <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './product.css';
+import { useStateValue } from './StateProvider';
 
-function Product({title,price,image,rating}) {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className='product'>
         <div className="product__info">
@@ -17,7 +34,7 @@ function Product({title,price,image,rating}) {
             </div>
             </div>
             <img src={image} alt="" />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
 }
