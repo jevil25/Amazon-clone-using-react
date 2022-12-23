865d8e26d8386c0434b6c50138e9188a8c27af5e
AaronNazareth <4nm20cs005@nmamit.in>
2022-12-23 10:46:40 +0530

    added new updates for firebase hosting

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,14 +5,17 @@ import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   // console.log(basket);
-
+  // console.log(user)
+  // let newuser=JSON.parse(JSON.stringify(user))
+  // console.log(newuser.email);
   return (
     <div className='Checkout'>
        <div className="checkout__left">
         <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
         <div>
+          {/* <h3>{user.email}</h3> */}
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map(item => (
             <CheckoutProduct 
--- src/Header.js
+++ src/Header.js
@@ -29,7 +29,7 @@ function Header() {
         <div className="header__nav">
         <Link to={!user && '/login'}>
             <div onClick={handleAuthentication} className='header__option'>
-                <span className='header__optionLineOne'>{user?'Hello':'Hello Guest'}</span>
+                <span className='header__optionLineOne'>{user?'Hello User':'Hello Guest'}</span>
                 <span className='header__optionLineTwo'>{user?'Sign Out':'Sign In'}</span>
             </div>
         </Link>
