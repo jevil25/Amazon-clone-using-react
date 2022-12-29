ed2683dc4d12dacaa6e6a2083a1b1273d44a2eec
AaronNazareth <4nm20cs005@nmamit.in>
2022-12-29 18:07:25 +0530

    added new component

---
+++ src/ProdDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProdDetails.css';
+
+function ProdDetails() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProdDetails
--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './product.css';
 import { useStateValue } from './StateProvider';
+import { Link } from 'react-router-dom';
+import ProdDetails from './ProdDetails';
 
 function Product({ id, title, image, price, rating }) {
+
+  function ProductDetails({ id, title, image, price, rating }){
+    return <ProdDetails 
+      id={id}
+      image={image}
+      title={title}
+      price={price}
+      rating={rating}
+      />
+  }
+
+
   console.log("this is product.js");
   const [{ basket,total }, dispatch] = useStateValue();
 
@@ -23,22 +37,24 @@ function Product({ id, title, image, price, rating }) {
   };
 
   return (
-    <div className='product'>
-        <div className="product__info">
-            <p>{title}</p>
-            <p className='product__price'>
-                <small>$</small>
-                <strong>{price}</strong>
-            </p>
-            <div className="product__rating">
-                {Array(rating).fill().map((_, i)=>(
-                    <p>⭐</p>
-                ))}
-            </div>
-            </div>
-            <img src={image} alt="" />
-            <button onClick={addToBasket}>Add to Basket</button>
-    </div>
+    // <Link to='/prodDetails'>
+      <div onClick={ProductDetails({ id, title, image, price, rating })} className='product'>
+          <div className="product__info">
+              <p>{title}</p>
+              <p className='product__price'>
+                  <small>$</small>
+                  <strong>{price}</strong>
+              </p>
+              <div className="product__rating">
+                  {Array(rating).fill().map((_, i)=>(
+                      <p>⭐</p>
+                  ))}
+              </div>
+              </div>
+              <img src={image} alt="" />
+              <button onClick={addToBasket}>Add to Basket</button>
+      </div>
+    // </Link>
   )
 }
 
